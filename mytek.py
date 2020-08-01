60225211c008972f81a8a4ec457c995b28c198b9
bornimation <66053257+bornimation@users.noreply.github.com>
2020-08-01 15:02:32 +0530

    Add files via upload
    This program is written for Linux os. You must have pdftocairo preinstalled in your machine. Check the python file for more details.

---
+++ mytek.py
@@ -0,0 +1,28 @@
+#This program takes tex command as input and output svg files. You must have texlive, pdftocairo installed in our system, the program is written for linux os. Written by Sasanka Dowarah.
+import os
+#Takes input from user.
+tex_equation = input("Type equation - ")
+#open files to write.
+tex_file = open('tex_file.tex','w')
+tex_file.writelines("\documentclass[tikz]{standalone}\n")
+tex_file.writelines("\\begin{document}\n")
+tex_file.writelines("\\begin{tikzpicture}\n")
+tex_file.writelines("\draw[line width=0mm] (0,0) rectangle (0,0) node [midway]\n")
+tex_file.writelines("{$")
+tex_file.writelines(tex_equation)
+tex_file.writelines("$};\n")
+tex_file.writelines("\end{tikzpicture}\n")
+tex_file.writelines("\end{document}\n")
+tex_file.close()
+#system command to compile the tex file.
+os.system('pdflatex tex_file.tex')
+#system command to convert the pdf file to svg file.
+os.system('pdftocairo -svg tex_file.pdf output.svg')
+#os.system('pdftocairo -svg tex_file.pdf output.png')
+#os.system('pdftocairo -svg tex_file.pdf output.jpeg')
+#os.system('pdftocairo -svg tex_file.pdf output.tirf')
+#os.system('pdftocairo -svg tex_file.pdf output.ps')
+#os.system('pdftocairo -svg tex_file.pdf output.eps')
+os.system('exit')
+print("Output is written in output.svg")
+
