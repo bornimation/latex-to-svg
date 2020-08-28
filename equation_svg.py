d50d9c87487542530f0282f7ba12efa8a1c72773
bornimation <66053257+bornimation@users.noreply.github.com>
2020-08-28 18:21:12 +0000

    comments removed

--- equation_svg.py
+++ equation_svg.py
@@ -1,15 +1,8 @@
-#This program takes tex command as input and output svg files. You must have texlive, pdftocairo installed in our system, the program is written for linux os. Written by Sasanka Dowarah.
+
 import os
 from PIL import Image
-#print("Runs on linux only. This program takes tex equation as input and produces svg file of the equation using LaTeX. Type your equation below as you type in a LaTeX compiler. The scale factor (non zero integer) scales your svg file by the input given. Typical values for scale input are 1(no scaling),10,100. Output will be written in output.svg file. Check the program for more info.\n")
 
-#if you want to use the svg files in blender, you should use scaling.
-#install texlive : sudo apt install texlive --full.
-#install pdftocairo : sudo apt install poppler-utils.
-#install PIL : pip install Pillow.
-#pillow is not required if you do not want scaling.
-#install rsvg convert : apt-get install librsvg2-bin.
-#rsvg package is not required if you do not want scaling.
+
 
 #Takes tex input from user.
 tex_equation = input("Type equation - ")
